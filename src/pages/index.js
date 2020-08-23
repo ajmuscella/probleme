3ed21c158a70d0ed653d54824659311ed297aaac
ajmuscella <ajmuscella@gmail.com>
2020-08-23 17:28:59 -0400

    removed spotify iFrame

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Video from "../../static/video.mp4"
 
 export default function Home() {
   return <div>
-  <div class="spotify">
-    <iframe class="spotify" src="https://open.spotify.com/embed/track/5SmX8PF2yeAeCimP2tmeV1" width="300" height="80" allowtransparency="true" allow="media"></iframe>
-  </div>
+  // <div class="spotify">
+  //   <iframe class="spotify" src="https://open.spotify.com/embed/track/5SmX8PF2yeAeCimP2tmeV1" width="300" height="80" allowtransparency="true" allow="media"></iframe>
+  // </div>
   <div class="contain">
     <video autoPlay muted loop id="bg" class="bg">
       <source src={Video} type="video/mp4" />
